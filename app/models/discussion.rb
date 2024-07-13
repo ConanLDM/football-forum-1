3175b765d4018094da4b9d1f44c3ca5d97dfa8b1
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-07-13 15:26:07 +0100

    to_param added to the discussion model
    In order to move away from a football discussion just
    being a numbered record in the URL, a to_param method
    has been set in the discussion model to take the discussion
    name, enforce parameters on it to separate words via hyphens and
    limit the URL title character count to 100 characters so that the URL
    does not grow to be too long.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,4 +3,8 @@ class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
   validates :title, presence: true
+
+  def to_param
+    "#{id}-#{title.downcase.to_s[0...100]}".parameterize
+  end
 end
