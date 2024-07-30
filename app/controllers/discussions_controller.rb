feac7fddeac3ad8b98bd2d74bb1107d2dfd26ee6
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-07-30 20:02:18 +0100

    Pagy Introduced to Discussions and Categories
    The Pagy gem has been added to the discussions and
    categories index pages via the index controller for each
    of them, this means on the frontend if there are categories
    and/or discussions present pagination will take effect and
    allow the user to choose between pages of records of discussions,
    either from all discussions or the discussions as part of a
    specific category.

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -3,7 +3,7 @@ class DiscussionsController < ApplicationController
   before_action :set_discussion, only: [:show, :edit, :destroy, :update]
 
   def index
-    @discussions = Discussion.all.order(updated_at: :desc)
+    @pagy, @discussions = pagy(Discussion.includes(:category))
   end
 
   def show
